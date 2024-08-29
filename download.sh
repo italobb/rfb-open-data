381ecb965ed9e707c65ecf99e66872bf02284f90
Italo Borssatto <italobb@users.noreply.github.com>
2024-08-29 23:07:38 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -6,16 +6,7 @@ cd tmp
 URL=https://dadosabertos.rfb.gov.br/CNPJ/dados_abertos_cnpj/$(date '+%Y-%m')/
 
 # Download public index
-files=$( \
-    curl -s $URL \
-    | grep external-link \
-    | sed -e's,^.*href="\(.*://[^"]*\).*$,\1,g' )
-
-for f in ${files//\\n/}
-do 
-    echo "$(basename $f)"
-    curl -s -L -C - -O $f &
-done
+wget --no-parent -r $URL
 
 wait $(jobs -p)
 
